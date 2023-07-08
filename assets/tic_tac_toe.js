16c1dbb4c533f3be38eaef1267131e2bb21c776e
Ricardoabra <66046872+Ricardoabra@users.noreply.github.com>
2023-07-07 22:02:06 -0300

    nova mudança radical

---
+++ assets/tic_tac_toe.js
@@ -0,0 +1,19 @@
+const tic_tac_toe = {
+    board:['','','','','','','','',''],
+    simbols:['x','o'],
+    container_element:null,
+
+    init: function(container){
+        this.container_element = container;
+    },
+
+    draw: function(){
+        let content = '';
+
+        for(i in this.board){
+            content += '<div>' +  i + '</div>';
+        }
+        this.container_element.innerHtml = content;
+    }
+
+};
